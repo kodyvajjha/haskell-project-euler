87c76fb5c1389a14329a21ec9667a9c84d8e297b
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-04-28 09:18:54 -0700

    problem 18

--- OCaml/lib/prob18.ml
+++ OCaml/lib/prob18.ml
@@ -1,1 +1,51 @@
-let () = print_endline "Hello, world!"
+(* let test = [ [ 3 ]; [ 7; 4 ]; [ 2; 4; 6 ]; [ 8; 5; 9; 3 ] ] *)
+
+let main =
+  [
+    [ 75 ];
+    [ 95; 64 ];
+    [ 17; 47; 82 ];
+    [ 18; 35; 87; 10 ];
+    [ 20; 04; 82; 47; 65 ];
+    [ 19; 01; 23; 75; 03; 34 ];
+    [ 88; 02; 77; 73; 07; 63; 67 ];
+    [ 99; 65; 04; 28; 06; 16; 70; 92 ];
+    [ 41; 41; 26; 56; 83; 40; 80; 70; 33 ];
+    [ 41; 48; 72; 33; 47; 32; 37; 16; 94; 29 ];
+    [ 53; 71; 44; 65; 25; 43; 91; 52; 97; 51; 14 ];
+    [ 70; 11; 33; 28; 77; 73; 17; 78; 39; 68; 17; 57 ];
+    [ 91; 71; 52; 38; 17; 14; 91; 43; 58; 50; 27; 29; 48 ];
+    [ 63; 66; 04; 68; 89; 53; 67; 30; 73; 16; 69; 87; 40; 31 ];
+    [ 04; 62; 98; 27; 23; 09; 70; 98; 73; 93; 38; 53; 60; 04; 23 ];
+  ]
+
+let rec tuplize l =
+  match l with
+  | [] -> []
+  | [ _ ] -> []
+  | x :: y :: ls -> (x, y) :: tuplize (y :: ls)
+
+let combine_tuplize l1 l2 =
+  if List.length l1 <> List.length l2 + 1 then
+    failwith
+      ("cannot tuplize lists of length "
+      ^ string_of_int (List.length l1)
+      ^ " and "
+      ^ string_of_int (List.length l2))
+  else
+    List.combine l2 (tuplize l1)
+
+let rec contract l =
+  match l with
+  | [] -> []
+  | [ x ] -> [ x ]
+  | l1 :: l2 :: ls ->
+    let l =
+      List.map (fun (a, (x, y)) -> max (a + x) (a + y)) (combine_tuplize l1 l2)
+    in
+    contract (l :: ls)
+
+let () =
+  CCFormat.printf "@[@.[%a]@]"
+    CCFormat.(list (list int))
+    (contract (List.rev main))
